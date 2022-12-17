85b6b28bdaf4d3ca8ba904c21b5225a0c9c1a44d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-12-17 11:22:00 -0600

    Add force flag

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+# check for force flag
+function has_param() {
+  local terms="$1"
+  shift
+
+  for term in $terms; do
+    for arg; do
+      if [[ $arg == "$term" ]]; then
+        echo "yes"
+      fi
+    done
+  done
+}
+
+# Allow to force deploy
+FORCE=$(has_param "-f --force" "$@")
+
 # clear dir before runs
 rm -rf out-web
 rm -rf out-api
 
 # build api
 buildOutput=$(npx turbo build --filter=api)
-if [[ $buildOutput != *"FULL TURBO"* ]]; then
+if [[ $buildOutput != *"FULL TURBO"* || $FORCE ]]; then
   echo "Deploying API..."
   npx turbo prune --scope=api --out-dir out-api
   npx firebase deploy --only functions
@@ -16,7 +33,7 @@ fi
 
 # build web
 buildOutput=$(npx turbo build --filter=web)
-if [[ $buildOutput != *"FULL TURBO"* ]]; then
+if [[ $buildOutput != *"FULL TURBO"* || $FORCE ]]; then
   echo "Deploying WEB..."
   npx turbo prune --scope=web --out-dir out-web
   npx firebase deploy --only hosting
