4d3bd14b2df15381a36f358b1c4124f6aa262b9f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-30 08:31:35 -0500

    Support v2 functions fix #5

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import * as functions from "firebase-functions";
 import { iLikeTurtles } from "@acme/shared";
+import { onRequest } from "firebase-functions/v2/https";
 import { faker } from "@faker-js/faker";
+import express from "express";
 
 const app = express();
 
-app.get("*", (req, res) => {
+app.get("*", (_req, res) => {
   res.send({
     hello: "world",
     randomName: faker.person.firstName(),
@@ -14,4 +14,4 @@ app.get("*", (req, res) => {
   });
 });
 
-export const server = functions.https.onRequest(app);
+export const server = onRequest(app);
--- apps/api/vite.config.ts
+++ apps/api/vite.config.ts
@@ -1,32 +1,36 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import generatePackageJson from "rollup-plugin-generate-package-json";
-import packageJson from "./package.json";
+import packageJson from "./package.json" assert { type: "json" }; 
+import { builtinModules } from "node:module";
 
-// NOTE: the @acme/shared package cannot be included into the deps casue it will attempt to be installed in 
+// NOTE: the @acme/shared package cannot be included into the deps casue it will attempt to be installed in
 // the firebase function build step and fail, so as a work around we are using a bundler to move the code into the final bundle that we ship to firebase
 
 // NOTE: we are using vite for bundling but "native" support for nodejs is not yet available
 // for the tracking of that there is the following issue and if that is solved this implementation may change
 // https://github.com/vitejs/vite/issues/7821
+
+const external = [...builtinModules, ...builtinModules.map((m) => `node:${m}`)];
+
 export default defineConfig({
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "firebase-functions-lib",
-      fileName: "index",
-      formats: ["es", "umd"],
-    },
+    ssr: "./src/index.ts",
     outDir: "dist",
     rollupOptions: {
-      external: Object.keys(packageJson.dependencies),
-      // NOTE: Firebase requires a package.json to be present in the functions folder
-      // this is a workaround to generate that file so firebase stays happy
+      output: {
+        inlineDynamicImports: true,
+      },
+      preserveSymlinks: true,
+      external,
       plugins: [
         generatePackageJson({
+          // @ts-ignore
           baseContents: packageJson,
         }),
       ],
     },
+  },
+  ssr: {
+    external,
   },
 });
